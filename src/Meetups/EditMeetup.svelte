c8c3bd9275b7738a8d42289920a762a32bb9c8d3
Brandon Franks <bgfranks.dev@gmail.com>
2021-12-25 19:55:05 -0500

    moved the add meetup form to a modal and added functions to close the modal either on click of the cancel button or clicking the modal backdrop

--- src/Meetups/EditMeetup.svelte
+++ src/Meetups/EditMeetup.svelte
@@ -3,6 +3,7 @@
   import { createEventDispatcher } from 'svelte';
   import TextInput from '../UI/TextInput.svelte';
   import Button from '../UI/Button.svelte';
+  import Modal from '../UI/Modal.svelte';
 
   // variables and constants
   let title = '';
@@ -26,55 +27,63 @@
       imgUrl,
     });
   }
+
+  // dispatches cancel
+  function cancel() {
+    dispatch('cancel');
+  }
 </script>
 
-<form on:submit|preventDefault={submitForm}>
-  <TextInput
-    id="title"
-    label="Title"
-    value={title}
-    on:input={(e) => (title = e.target.value)}
-  />
-  <TextInput
-    id="subtitle"
-    label="Subtitle"
-    value={subtitle}
-    on:input={(e) => (subtitle = e.target.value)}
-  />
-  <TextInput
-    id="location"
-    label="Location"
-    value={location}
-    on:input={(e) => (location = e.target.value)}
-  />
-  <TextInput
-    id="imgUrl"
-    label="Image URL"
-    value={imgUrl}
-    on:input={(e) => (imgUrl = e.target.value)}
-  />
-  <TextInput
-    id="contactEmail"
-    label="Email"
-    type="email"
-    value={contactEmail}
-    on:input={(e) => (contactEmail = e.target.value)}
-  />
-  <TextInput
-    id="description"
-    label="Description"
-    value={description}
-    controlType="textarea"
-    rows="3"
-    on:input={(e) => (description = e.target.value)}
-  />
-  <Button type="submit" caption="Save" />
-</form>
+<Modal title="Edit Meetup Data" on:cancel>
+  <form on:submit|preventDefault={submitForm}>
+    <TextInput
+      id="title"
+      label="Title"
+      value={title}
+      on:input={(e) => (title = e.target.value)}
+    />
+    <TextInput
+      id="subtitle"
+      label="Subtitle"
+      value={subtitle}
+      on:input={(e) => (subtitle = e.target.value)}
+    />
+    <TextInput
+      id="location"
+      label="Location"
+      value={location}
+      on:input={(e) => (location = e.target.value)}
+    />
+    <TextInput
+      id="imgUrl"
+      label="Image URL"
+      value={imgUrl}
+      on:input={(e) => (imgUrl = e.target.value)}
+    />
+    <TextInput
+      id="contactEmail"
+      label="Email"
+      type="email"
+      value={contactEmail}
+      on:input={(e) => (contactEmail = e.target.value)}
+    />
+    <TextInput
+      id="description"
+      label="Description"
+      value={description}
+      controlType="textarea"
+      rows="3"
+      on:input={(e) => (description = e.target.value)}
+    />
+  </form>
+  <div slot="footer">
+    <Button mode="outline" caption="Cancel" on:click={cancel} />
+    <Button caption="Save" on:click={submitForm} />
+  </div>
+</Modal>
 
 <style>
   form {
-    width: 30rem;
-    max-width: 90%;
-    margin: auto;
+    width: 100%;
   }
 </style>
