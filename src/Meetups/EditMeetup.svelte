9c8be5ee77741839924838d5759b8ec16f425d6a
Brandon Franks <bgfranks.dev@gmail.com>
2021-12-26 20:56:11 -0500

    added form validation the the meetup form along with custom validation for the email address and image URL

--- src/Meetups/EditMeetup.svelte
+++ src/Meetups/EditMeetup.svelte
@@ -4,6 +4,7 @@
   import TextInput from '../UI/TextInput.svelte';
   import Button from '../UI/Button.svelte';
   import Modal from '../UI/Modal.svelte';
+  import { isEmpty, isValidEmail, isValidHttpUrl } from '../Helpers/validation';
 
   // variables and constants
   let title = '';
@@ -15,6 +16,21 @@
 
   // creates event dispatcher
   const dispatch = createEventDispatcher();
+
+  // binds data to the validation functions
+  $: titleValid = !isEmpty(title);
+  $: subtitleValid = !isEmpty(subtitle);
+  $: locationValid = !isEmpty(location);
+  $: emailValid = isValidEmail(contactEmail);
+  $: descValid = !isEmpty(description);
+  $: imgValid = isValidHttpUrl(imgUrl);
+  $: formIsValid =
+    titleValid &&
+    subtitleValid &&
+    locationValid &&
+    emailValid &&
+    descValid &&
+    imgValid;
 
   // function to save the data from the form to the save dispatch event
   function submitForm() {
@@ -40,24 +56,32 @@
       id="title"
       label="Title"
       value={title}
+      valid={titleValid}
+      errMsg="Please enter a valid title"
       on:input={(e) => (title = e.target.value)}
     />
     <TextInput
       id="subtitle"
       label="Subtitle"
       value={subtitle}
+      valid={subtitleValid}
+      errMsg="Please enter a valid subtitle"
       on:input={(e) => (subtitle = e.target.value)}
     />
     <TextInput
       id="location"
       label="Location"
       value={location}
+      valid={locationValid}
+      errMsg="Please enter a valid location"
       on:input={(e) => (location = e.target.value)}
     />
     <TextInput
       id="imgUrl"
       label="Image URL"
       value={imgUrl}
+      valid={imgValid}
+      errMsg="Please enter a valid image URL"
       on:input={(e) => (imgUrl = e.target.value)}
     />
     <TextInput
@@ -65,12 +89,16 @@
       label="Email"
       type="email"
       value={contactEmail}
+      valid={emailValid}
+      errMsg="Please enter a valid email address"
       on:input={(e) => (contactEmail = e.target.value)}
     />
     <TextInput
       id="description"
       label="Description"
       value={description}
+      valid={descValid}
+      errMsg="Please enter a valid meetup description"
       controlType="textarea"
       rows="3"
       on:input={(e) => (description = e.target.value)}
@@ -78,7 +106,7 @@
   </form>
   <div slot="footer">
     <Button mode="outline" caption="Cancel" on:click={cancel} />
-    <Button caption="Save" on:click={submitForm} />
+    <Button caption="Save" on:click={submitForm} disabled={!formIsValid} />
   </div>
 </Modal>
 
--- src/UI/Button.svelte
+++ src/UI/Button.svelte
@@ -5,12 +5,13 @@
   export let href = null;
   export let mode = null;
   export let color = null;
+  export let disabled = false;
 </script>
 
 {#if href}
   <a {href}>{caption}</a>
 {:else}
-  <button class="{mode} {color}" {type} on:click>{caption}</button>
+  <button class="{mode} {color}" {type} {disabled} on:click>{caption}</button>
 {/if}
 
 <style>
